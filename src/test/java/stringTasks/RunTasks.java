a738c0d429653e0739ccf4dd0e3ae28015e1e862
Angelina <angelinafedcenko@gmail.com>
2021-06-24 14:02:50 +0300

    Strings

---
+++ src/test/java/stringTasks/RunTasks.java
@@ -0,0 +1,38 @@
+package stringTasks;
+
+public class RunTasks {
+    public static void main(String[] args) {
+        Tasks stringTasks = new Tasks();
+
+        System.out.println("=1=");
+        stringTasks.getByIndex();
+
+        System.out.println("=2=");
+        stringTasks.getUnitCode();
+
+        System.out.println("=3=");
+        stringTasks.concatStrings();
+
+        System.out.println("=4=");
+        stringTasks.compareSequence();
+
+        System.out.println("=5=");
+        stringTasks.compareObjects();
+
+        System.out.println("=6=");
+        stringTasks.getIndexOfString();
+
+        System.out.println("=7=");
+        stringTasks.replaceSymbols();
+
+        System.out.println("=8=");
+        stringTasks.getSubString();
+
+        System.out.println("=9=");
+        stringTasks.convertToLowercase();
+
+
+
+
+    }
+}
